ea46310dd0e559b0ac651091927f6024a5565801
Mattan Ingram <mattaningram@gmail.com>
2015-01-31 17:40:45 -0500

    oops wp update with ga fix

--- blog/wp-content/themes/mashwork2014/footer.php
+++ blog/wp-content/themes/mashwork2014/footer.php
@@ -5,6 +5,7 @@
  */
 ?>
 
+	
 <footer class="page-footer">
   <ul class="social-links">
     <li class="twitter-link"><a href="https://www.twitter.com/mashwork" target="_blank"><img src="/blog/wp-content/themes/mashwork2014/images/twitter-icon.svg" class="social-icon"/></a></li>
@@ -14,21 +15,6 @@
   <p class="copyright">&copy; 2014 Mashwork, Inc. &nbsp;•&nbsp; All rights reserved.</p>
   <p class="contact-info"><a href="https://goo.gl/maps/slKfo" target="_blank">11 W. 25th St, 7th Fl, New York, NY 10010</a>• <a href="tel:1-646-201-9124">646-201-9124</a></p><a href="http://wearemadeinny.com/" target="_blank" class="made-in-ny"><img src="/blog/wp-content/themes/mashwork2014/images/logos/made-in-ny-logo.svg"/></a>
 </footer>
-	<!-- Google Analytics -->
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-16801378-1']);
-  _gaq.push(['_setSiteSpeedSampleRate', 50]);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
 	<?php wp_footer(); ?>
 
 </body>
